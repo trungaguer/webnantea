1c00011e717563813077205e5f9fe8f38318c1ed
trungvo4229-cell <trungvo4229@gmail.com>
2026-04-23 02:40:01 +0700

    fix cors for vercel

--- my_web_backend/src/index.js
+++ my_web_backend/src/index.js
@@ -16,7 +16,7 @@ const port = process.env.PORT || 3001;
 // ✅ CORS
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://webnantea-3b49.vercel.app"],
     credentials: true,
   }),
 );
